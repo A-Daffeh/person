036cd98c6e5494027143be5e152c99ee19c8c1ea
Alagie F. Nget <Alagie10@gmail.com>
2022-06-15 19:20:48 -0500

    Minor fix

--- src/main/java/com/cs544/project/person/repository/IUserDao.java
+++ src/main/java/com/cs544/project/person/repository/IUserDao.java
@@ -2,7 +2,9 @@ package com.cs544.project.person.repository;
 
 import com.cs544.project.person.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserDao extends JpaRepository<User, Long> {
 
 }
